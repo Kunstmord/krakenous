5e0eac2b10e9668bd1421e4c7b950c1f7425488f
georgeoblapenko <kunstmord@gmail.com>
2014-05-07 14:08:34 +0400

    added some imports

--- src/backends.py
+++ src/backends.py
@@ -1,4 +1,6 @@
 __author__ = 'George Oblapenko, Viktor Evstratov'
 __license__ = "GPLv3"
 #sqlite, mongodb, csv writes go here. How to organize this? try: import pymongo except smth?
-# write_<backend>, read_<backend>, rename_<backend>?
+# write_<backend>, read_<backend>, rename_<backend>?]
+from csv import reader, writer
+import sqlite3
