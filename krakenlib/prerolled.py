443ba8b68242c4c080af48948357785da007abca
georgeoblapenko <kunstmord@gmail.com>
2014-05-07 16:49:17 +0400

    add_labels nearly done, dump_dataset added

--- krakenlib/prerolled.py
+++ krakenlib/prerolled.py
@@ -7,6 +7,7 @@ sync/dump/copy datasets
 from csv import reader
 from os.path import join, isfile
 from os import walk
+from krakenlib.dataset import DataSet
 
 
 def csv_tentacle(path_to_file, id_column_number, missing_element=-1, custom_mapping: list=None):
@@ -32,11 +33,37 @@ def folder_tentacle(path_to_folder, column_name: str, exclude_files_list: tuple=
                         yield {column_name: filename_transform(f_name, **kwargs)}
 
 
-def dump_dataset(dataset, dump_path, dump_backend):
-    pass
+def add_labels(dataset: DataSet, id_field_name: str, labels, labels_name: str='label', overwrite_existing: bool=False):
+    """
+    labels - iterable of iterables - ((some_id, label), ...)
+    return difference between added and not_added?
+    """
+    # if overwrite_existing is False and dataset.check_feature_existence(labels_name) is True:
+    #     raise someError
+
+    inserts = 0
+
+    for data_record in dataset.yield_data_records(('id', id_field_name)):
+        for single_label in labels:
+            if data_record['id_field_name'] in single_label[0]:
+                dataset.insert_single(data_record['id'], labels_name, single_label[1], True)
+                inserts += 1
+    return dataset.total_records - inserts
 
 
-def copy_to_dataset(dataset1, dataset2):
+def dump_dataset(dataset: DataSet, dump_path: str, dump_backend: str, column_names: tuple=(), copy_meta: bool=True):
+    """
+    allow copying only of specified column_names
+    """
+    new_dataset = DataSet(dump_path, dump_backend)
+    if copy_meta is True:
+        new_dataset.metadata = dataset.metadata
+    for data_record in dataset.yield_data_records(column_names):
+        new_dataset.append_data_record(data_record)
+    return None
+
+
+def copy_to_dataset(dataset1: DataSet, dataset2: DataSet, overwrite: bool=False):
     pass
 
 
