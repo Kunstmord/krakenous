98a1a6bbd87a56b076fc4e5c44a0d175ea30034b
georgeoblapenko <kunstmord@gmail.com>
2014-06-08 23:51:49 +0400

    shelve added, small comment clean-up

--- krakenlib/backends.py
+++ krakenlib/backends.py
@@ -1,6 +1,8 @@
 __author__ = 'George Oblapenko, Viktor Evstratov'
 __license__ = "GPLv3"
-#sqlite, mongodb, csv writes go here. How to organize this? try: import pymongo except smth?
-# write_<backend>, read_<backend>, rename_<backend>?]
+# sqlite, shelve, csv (for dumps) go here
+
+
 from csv import reader, writer
 import sqlite3
+import shelve
