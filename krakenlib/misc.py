e52975e63e9d45b0956dd44c9d8b648224f055ff
georgeoblapenko <kunstmord@gmail.com>
2014-06-13 15:08:19 +0400

    forgot to import numpy

--- krakenlib/misc.py
+++ krakenlib/misc.py
@@ -3,6 +3,8 @@ __author__ = 'George Oblapenko'
 Misc. helper functions
 """
 
+import numpy as np
+
 
 def is_a_number(x):
     if isinstance(x, int) or isinstance(x, float) or isinstance(x, complex) or isinstance(x, np.number):
