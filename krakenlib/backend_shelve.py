db723aa9aff546ed20326e1549dee43c6b7d7f9b
George Oblapenko <kunstmord@gmail.com>
2014-12-15 12:17:07 +0300

    Changed indexing and writebacks
    Indexes are now integers instead of string, a 'writeback' parameter is now stored in the db dictionary. Some doc strings added, read_data updated.

--- krakenlib/backend_shelve.py
+++ krakenlib/backend_shelve.py
@@ -1,7 +1,7 @@
 __author__ = 'George Oblapenko'
 __license__ = "GPLv3"
 """
-The structure is {str(id): {'feature1': feature1, ...}, ...}
+The structure is {id: {'feature1': feature1, ...}, ...}
 """
 
 import shelve
@@ -13,8 +13,8 @@ def open_db(db_data: dict, writeback: bool=False) -> dict:
     """
     open the db for reading, return everything in a dict
     """
-
-    return {'db': shelve.open(db_data['shelve_path'], writeback=writeback)}
+    return {'db': shelve.open(db_data['shelve_path'], writeback=writeback),
+            'writeback': writeback}
 
 
 def close_db(db: dict):
@@ -27,76 +27,98 @@ def commit_db(db: dict):
 
 
 def write_data(db: dict, record_id: int, data_name: str, data):
+    """Write data for a given record_id
+    """
     if not db['db'].keys():
-        db['db'][str(record_id)] = {}
-    elif str(record_id) not in db['db']:
-        db['db'][str(record_id)] = {}
-    if not db['db'][str(record_id)].keys():
-        db['db'][str(record_id)] = {data_name: data}
+        db['db'][record_id] = {}
+    elif record_id not in db['db']:
+        db['db'][record_id] = {}
+    if not db['db'][record_id].keys():
+        db['db'][record_id] = {data_name: data}
     else:
-        if db['db'].__getattribute__('writeback') is True:
-            db['db'][str(record_id)][data_name] = data
+        if db['writeback']:
+            db['db'][record_id][data_name] = data
         else:
-            tmp_dict = db['db'][str(record_id)]
+            tmp_dict = db['db'][record_id]
             tmp_dict[data_name] = data
-            db['db'][str(record_id)] = tmp_dict
+            db['db'][record_id] = tmp_dict
 
 
 def read_data(db: dict, record_id: int, data_name: str):
-    return db['db'][str(record_id)][data_name]
+    """Return the contents of a data column specified by data_name for a given record_id
+    """
+    if data_name in db['db'][record_id].keys():
+        return db['db'][record_id][data_name]
+    else:
+        return None
 
 
 def read_all_data(db: dict, record_id: int):
+    """
+    Return all data for a given record_id
+    """
     return_dict = {}
-    for data_name in db['db'][str(record_id)]:
-        return_dict[data_name] = db['db'][str(record_id)][data_name]
+    for data_name in db['db'][record_id]:
+        return_dict[data_name] = db['db'][record_id][data_name]
     return return_dict
 
 
 def delete_data(db: dict, record_id: int, data_name: str):
-    if data_name in db['db'][str(record_id)].keys():  # we check whether the column exists for the current record
-        if db['db'].__getattribute__('writeback') is True:
-            del db['db'][str(record_id)][data_name]
+    """Delete data for a given column name and record_id
+    """
+    if data_name in db['db'][record_id].keys():
+        if db['writeback']:
+            del db['db'][record_id][data_name]
         else:
-            tmp_dict = db['db'][str(record_id)]
+            tmp_dict = db['db'][record_id]
             del tmp_dict[data_name]
-            db['db'][str(record_id)] = tmp_dict
+            db['db'][record_id] = tmp_dict
 
 
 def data_exists(db: dict, record_id: int, data_name: str) -> bool:
-    if db['db'][str(record_id)] == {}:
+    """Check if data exists for a given column name and record_id
+    """
+    if db['db'][record_id] == {}:
         return False
-    elif data_name not in db['db'][str(record_id)].keys():
+    elif data_name not in db['db'][record_id].keys():
         return False
     else:
         return True
 
 
 def data_names(db: dict, record_id: int) -> list:
-    if db['db'][str(record_id)] == {}:
+    """Return data names for a given record
+    """
+    # TODO - remove element_length
+    if db['db'][record_id] == {}:
         return []
     else:
         return_list = []
-        for data_name in db['db'][str(record_id)]:
-            feature_len = element_length(db['db'][str(record_id)][data_name])[0]
+        for data_name in db['db'][record_id]:
+            feature_len = element_length(db['db'][record_id][data_name])[0]
             return_list.append((data_name, feature_len))
         return return_list
 
 
 def data_length_and_type(db: dict, record_id: int, data_name: str):
-    if db['db'][str(record_id)] == {}:
+    # TODO - Deprecate
+    if db['db'][record_id] == {}:
         return -1
     else:
-        return element_length(db['db'][str(record_id)][data_name])
+        return element_length(db['db'][record_id][data_name])
 
 
 def delete_record(db: dict, record_id: int, total_records):
+    """Delete a record specified by record_id
+    """
     for i in range(record_id, total_records):
-        db['db'][str(record_id)] = db['db'][str(record_id + 1)]
-    del db['db'][str(total_records)]
+        db['db'][record_id] = db['db'][record_id + 1]
+    del db['db'][total_records]
 
 
 def data_records_amount(db_data: dict) -> int:
+    """How many data records are there in all?
+    """
     db = shelve.open(db_data['shelve_path'])
     records_amount = len(db.keys())
     db.close()
