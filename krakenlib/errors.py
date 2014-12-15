2660c39a77967ce4b4bf674374feb9870fd55170
georgeoblapenko <kunstmord@gmail.com>
2014-12-15 20:29:07 +0300

    simplified exceptions

--- krakenlib/errors.py
+++ krakenlib/errors.py
@@ -1,47 +1,7 @@
 __author__ = 'George Oblapenko, Viktor Evstratov'
 __license__ = "GPLv3"
-# Custom erros (no backend found, no such id, cannot add to db, field with this name already exists, etc) go here
-
-
-class UnsupportedOperation(Exception):
-    pass
-
-
-class OverwriteError(Exception):
-    pass
-
-
-class UnknownBackend(Exception):
-    pass
-
-
-class DoesNotExist(Exception):
-    pass
-
-
-class DataPropertyNameCollision(Exception):
-    pass
-
-
-class ProtectedField(Exception):
-    pass
+# Custom errors (no backend found, no such id, cannot add to db, field with this name already exists, etc) go here
 
 
 class KrakenlibException(Exception):
-    pass
-
-
-class EmptyDataSet(Exception):
-    pass
-
-
-class InconsistentFeatureTypes(Exception):
-    pass
-
-
-class NonNumericFeatureType(Exception):
-    pass
-
-
-class IncorrectRange(Exception):
     pass
