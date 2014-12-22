38162325dd2699629f0c14a077c4123d25cdfe28
georgeoblapenko <kunstmord@gmail.com>
2014-12-22 20:59:23 +0300

    moved numpy conversion tests, fixed some calls, all tests pass

--- krakenlib/tests.py
+++ krakenlib/tests.py
@@ -1,7 +1,7 @@
 __author__ = 'Viktor Evstratov, George Oblapenko'
 import unittest
 from krakenlib.dataset import DataSet
-from krakenlib.prerolled import folder_tentacle
+from krakenlib.prerolled import folder_tentacle, convert_multiple_features_numpy, convert_single_feature_numpy
 import os.path
 from os import makedirs
 import numpy as np
@@ -53,21 +53,21 @@ class TestShelve(unittest.TestCase):
 
     def test_extraction_value(self):
         self.dataset.extract_feature(fake_extractor, 46, overwrite_feature=True)
-        assert self.dataset.return_single_data_record(1, ('fake_extractor',))['fake_extractor'] == 46
+        assert self.dataset.single_data_record(1, ('fake_extractor',))['fake_extractor'] == 46
         # the feature value is correct
         self.dataset.extract_feature(fake_extractor, 'meaning of life', overwrite_feature=True)
-        assert self.dataset.return_single_data_record(1, ('fake_extractor',))['fake_extractor'] == 'meaning of life'
+        assert self.dataset.single_data_record(1, ('fake_extractor',))['fake_extractor'] == 'meaning of life'
         # feature overwriting works correctly
 
     def test_insert_single_value(self):
         self.dataset.insert_single(5, 'extra_field', 'extra number', overwrite_existing=True)
         assert self.dataset.feature_exists(5, 'extra_field') is True  # inserts something
         assert self.dataset.feature_exists(4, 'extra_field') is False  # inserts only for the correct id
-        self.assertRaises(krakenlib.errors.DataPropertyNameCollision, self.dataset.insert_single,
+        self.assertRaises(krakenlib.errors.KrakenlibException, self.dataset.insert_single,
                           5, 'extra_field', 3333)
         # ^ does not overwrite by accident and raies correct error
         self.dataset.insert_single(5, 'extra_field', 5555, overwrite_existing=True)
-        assert self.dataset.return_single_data_record(5, ('extra_field',))['extra_field'] == 5555
+        assert self.dataset.single_data_record(5, ('extra_field',))['extra_field'] == 5555
         # overwrite works correctly
 
     def test_delete_feature(self):
@@ -83,66 +83,72 @@ class TestShelve(unittest.TestCase):
         self.dataset.rename_feature('a_very_fake_extractor', 'renamed_fake_extractor')
         assert self.dataset.feature_exists_global('a_very_fake_extractor') is False  # original name doesn't exists
         assert self.dataset.feature_exists_global('renamed_fake_extractor') is True  # new name exists
-        assert self.dataset.return_single_data_record(3, ('ext3',))['ext3'] == 23930  # original value is correct
+        assert self.dataset.single_data_record(3, ('ext3',))['ext3'] == 23930  # original value is correct
         self.dataset.rename_feature('renamed_fake_extractor', 'ext3', overwrite_existing=True, writeback=3)
         assert self.dataset.feature_exists_global('renamed_fake_extractor') is False  # new name doesn't exist
         assert self.dataset.feature_exists_global('ext3') is True  # newest name still exists
-        assert self.dataset.return_single_data_record(3, ('ext3',))['ext3'] == 30239  # and the value was overwritten
+        assert self.dataset.single_data_record(3, ('ext3',))['ext3'] == 30239  # and the value was overwritten
 
     def test_feature_names(self):
         self.dataset.extract_feature(a_very_fake_extractor, overwrite_feature=True)
         self.dataset.extract_feature(numpy_feature, overwrite_feature=True)
         self.dataset.extract_feature(string_feature, overwrite_feature=True)
-        feature_names = self.dataset.return_feature_names()
-        names = []
-        lengths = []
-        for i in feature_names:
-            names.append(i[0])
-            lengths.append(i[1])
-        assert 'a_very_fake_extractor' in names
-        assert 'string_feature' in names
-        assert 'numpy_feature' in names  # the features were returned
-        fake_index = names.index('a_very_fake_extractor')
-        string_index = names.index('string_feature')
-        numpy_index = names.index('numpy_feature')
-        assert lengths[fake_index] == 0
-        assert lengths[string_index] == 'string'
-        assert lengths[numpy_index] == 3  # along with their correct lengths/types
+        feature_names = self.dataset.feature_names()
+        assert 'a_very_fake_extractor' in feature_names
+        assert 'string_feature' in feature_names
+        assert 'numpy_feature' in feature_names  # the features were returned
 
     def test_return_single_feature(self):
-        feature = self.dataset.return_single_feature('string_feature')
+        feature = self.dataset.single_feature('string_feature')
         assert len(feature) == self.dataset.total_records  # default values work correctly, returns data for all records
         assert feature[0] == 'test string feature'  # returns the correct data value
-        feature = self.dataset.return_single_feature('string_feature', start_id=3)
+        feature = self.dataset.single_feature('string_feature', start_id=3)
         assert len(feature) == self.dataset.total_records - 2
-        feature = self.dataset.return_single_feature('string_feature', start_id=4, end_id=7)
+        feature = self.dataset.single_feature('string_feature', start_id=4, end_id=7)
         assert len(feature) == 4  # and the start_id, end_id parameters work correctly
 
+    def test_append_and_delete_data_record(self):
+        current_records = self.dataset.total_records
+        self.dataset.append_data_record({'fake_extractor': 'NOT THE MEANING OF LIFE AT ALL'})
+        assert self.dataset.total_records == current_records + 1  # the number of records changes
+        assert self.dataset.single_data_record(current_records + 1, ('fake_extractor',))['fake_extractor']\
+               == 'NOT THE MEANING OF LIFE AT ALL'  # the data in the record is correct
+        assert self.dataset.single_data_record(2, ('fake_extractor',))['fake_extractor']\
+               != 'NOT THE MEANING OF LIFE AT ALL'  # the data in other record doesn't change
+        self.dataset.delete_records((self.dataset.total_records,))
+        assert self.dataset.total_records == current_records  # the number of records changes back to the original
+        assert self.dataset.single_data_record(current_records, ('fake_extractor',))['fake_extractor']\
+               != 'NOT THE MEANING OF LIFE AT ALL'  # the last record is correct
+
+
+class TestNumpyConvert(unittest.TestCase):
+    def setUp(self):
+        """
+        This creates a DataSet which uses the shelve backend. It also creates a folder with 10 files (named 0 to 9)
+        to use with the folder_tentacle (if a testfolder already exists, this just reaches inside it and grabs what's
+        there)
+        """
+        self.dataset = DataSet('shelve', {'shelve_path': 'testshelve'})  # we will use the folder tentacle for testing
+        if self.dataset.total_records == 0:
+            if not os.path.isdir('testfolder'):
+                makedirs('testfolder')
+                for i in range(10):
+                    open('testfolder/' + str(i), 'a').close()
+            self.dataset.populate(folder_tentacle, 'testfolder', 'filename')
+
     def test_return_single_feature_numpy(self):
-        feature = self.dataset.return_single_feature_numpy('numpy_feature')
+        feature = convert_single_feature_numpy(self.dataset, 'numpy_feature')
         assert feature.shape == (self.dataset.total_records, 3,)
         assert feature[0, 1] == 0  # converts a single feature for each record into a single numpy array correctly
 
     def test_return_multiple_features_numpy(self):
         self.dataset.extract_feature(numpy_feature2, overwrite_feature=True)
         assert self.dataset.feature_exists_global('numpy_feature2') is True
-        feature = self.dataset.return_multiple_features_numpy(('numpy_feature', 'numpy_feature2',))
+        feature = convert_multiple_features_numpy(self.dataset, ('numpy_feature', 'numpy_feature2',))
+        assert isinstance(feature, np.ndarray) == True
         assert feature.shape == (self.dataset.total_records, 5,)
         assert feature[0, 1] == 0
         assert feature[0, 4] == 1  # converts multiple features for each record into a single numpy array correctly
-
-    def test_append_and_delete_data_record(self):
-        current_records = self.dataset.total_records
-        self.dataset.append_data_record({'fake_extractor': 'NOT THE MEANING OF LIFE AT ALL'})
-        assert self.dataset.total_records == current_records + 1  # the number of records changes
-        assert self.dataset.return_single_data_record(current_records + 1, ('fake_extractor',))['fake_extractor']\
-               == 'NOT THE MEANING OF LIFE AT ALL'  # the data in the record is correct
-        assert self.dataset.return_single_data_record(2, ('fake_extractor',))['fake_extractor']\
-               != 'NOT THE MEANING OF LIFE AT ALL'  # the data in other record doesn't change
-        self.dataset.delete_records((self.dataset.total_records,))
-        assert self.dataset.total_records == current_records  # the number of records changes back to the original
-        assert self.dataset.return_single_data_record(current_records, ('fake_extractor',))['fake_extractor']\
-               != 'NOT THE MEANING OF LIFE AT ALL'  # the last record is correct
 
 
 if __name__ == '__main__':
