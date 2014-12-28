48ca0fb38f4299fa6cb7d444aa110a4178bacfa5
georgeoblapenko <kunstmord@gmail.com>
2014-12-28 20:34:07 +0300

    some errors fixed and a couple of functions added

--- krakenlib/backend_sqlite.py
+++ krakenlib/backend_sqlite.py
@@ -10,9 +10,11 @@ from json import loads, dumps
 
 def open_db(db_data: dict, writeback: bool=False) -> dict:
     """
-    open the db for reading, return everything in a dict
+    open the db for reading, return everything in a dict - cursor, connection, metadata
     """
-    return {'db': sqlite3.connect(db_data['db_path']).cursor(),
+    conn = sqlite3.connect(db_data['db_path'])
+    return {'db': conn.cursor(),
+            'db_connection': conn,
             'writeback': writeback, 'table_name': db_data['table_name']}
 
 
@@ -22,7 +24,7 @@ def close_db(db: dict):
 
 
 def commit_db(db: dict):
-    db['db'].commit()
+    db['db_connection'].commit()
 #
 #
 # def write_data(db: dict, record_id: int, data_name: str, data):
@@ -48,13 +50,9 @@ def read_single_data(db: dict, record_id: int, data_name: str):
     """Return the contents of a data column specified by data_name for a given record_id
     """
     db['db'].execute('SELECT :data_name FROM :tablename WHERE id=:record_id', {'data_name': data_name,
-                                                                                     'tablename': db['table_name'],
-                                                                                     'record_id': record_id})
-    return loads(tuple(db['db'].fetchone())[0])
-    # if data_name in db['db'][str(record_id)].keys():
-    #     return db['db'][str(record_id)][data_name]
-    # else:
-    #     return None
+                                                                               'tablename': db['table_name'],
+                                                                               'record_id': record_id})
+    return loads(db['db'].fetchone()[0])
 
 
 def read_multiple_data(db: dict, record_id: int, data_names: tuple):
@@ -70,75 +68,47 @@ def read_all_data(db: dict, record_id: int):
     """
     Return the contents of all data columns for a given record_id
     """
-    # return_dict = {}
-    # for data_name in db['db'][str(record_id)]:
-    #     return_dict[data_name] = db['db'][str(record_id)][data_name]
-    # return return_dict
-    result = {}
     db['db'].execute('SELECT * FROM :tablename WHERE id=:record_id', {'tablename': db['table_name'],
                                                                       'record_id': record_id})
 
     res = db['db'].fetchone()
-    res_keys = res.keys()
-    for key in res_keys:
-        result[key] = loads(res[key])
-    return result
-    # return loads(res.fetchone())
-#
-#
-# def delete_data(db: dict, record_id: int, data_name: str):
-#     """Delete data for a given column name and record_id
-#     """
-#     if data_name in db['db'][str(record_id)].keys():
-#         if db['writeback']:
-#             del db['db'][str(record_id)][data_name]
-#         else:
-#             tmp_dict = db['db'][str(record_id)]
-#             del tmp_dict[data_name]
-#             db['db'][str(record_id)] = tmp_dict
-#
-#
-# def data_exists(db: dict, record_id: int, data_name: str) -> bool:
-#     """Check if data exists for a given column name and record_id
-#     """
-#     if db['db'][str(record_id)] == {}:
-#         return False
-#     elif data_name not in db['db'][str(record_id)].keys():
-#         return False
-#     else:
-#         return True
-#
-#
+    return list(map(lambda x: loads(x), res[1:]))
+
+
+def data_exists(db: dict, record_id: int, data_name: str) -> bool:
+    """Check if data exists for a given column name and record_id
+    """
+    db['db'].execute('SELECT :data_name FROM :tablename WHERE id=:record_id', {'data_name': data_name,
+                                                                               'tablename': db['table_name'],
+                                                                               'record_id': record_id})
+    if not db['db'].fetchone()[0]:
+        return False
+    else:
+        return True
+
 
 def all_data_names(db: dict) -> list:
     """Return data column names
     """
     db['db'].execute('SELECT * FROM :tablename', {'tablename': db['table_name']})
+    return list(map(lambda x: x[0], db['db'].description))
 
-    res = db['db'].fetchone()
-    res_keys = res.keys()
-    return res_keys
-    # if db['db'][str(record_id)] == {}:
-    #     return []
-    # else:
-    #     return_list = []
-    #     for data_name in db['db'][str(record_id)]:
-    #         return_list.append(data_name)
-    #     return return_list
-#
-#
-# def delete_record(db: dict, record_id: int, total_records):
-#     """Delete a record specified by record_id
-#     """
-#     for i in range(record_id, total_records):
-#         db['db'][str(record_id)] = db['db'][record_id + 1]
-#     del db['db'][str(total_records)]
-#
-#
-# def data_records_amount(db_data: dict) -> int:
-#     """How many data records are there in all?
-#     """
-#     db = shelve.open(db_data['shelve_path'])
-#     records_amount = len(db.keys())
-#     db.close()
-#     return records_amount
+
+def delete_record(db: dict, record_id: int, total_records):
+    """Delete a record specified by record_id
+    """
+    db['db'].execute('DELETE FROM :tablename WHERE id=:record_id', {'tablename': db['table_name'],
+                                                                    'record_id': record_id})
+
+    # for i in range(record_id, total_records):
+    #     db['db'][str(record_id)] = db['db'][record_id + 1]
+    del db['db'][str(total_records)]
+
+
+def data_records_amount(db_data: dict) -> int:
+    """How many data records are there in all?
+    """
+    conn = sqlite3.connect(db_data['db_path'])
+    c = conn.cursor()
+    c.execute('SELECT Count(*) FROM :tablename', {'tablename': db_data['table_name']})
+    return c.fetchone()[0]
