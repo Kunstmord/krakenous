615700fda0c327baf14ceb2e20c6132250967c45
georgeoblapenko <kunstmord@gmail.com>
2014-12-29 21:17:09 +0300

    KrakenlibException renamed to KrakenousException, added KrakenousNotImplemented

--- krakenlib/errors.py
+++ krakenlib/errors.py
@@ -3,5 +3,9 @@ __license__ = "GPLv3"
 # Custom errors (no backend found, no such id, cannot add to db, field with this name already exists, etc) go here
 
 
-class KrakenlibException(Exception):
+class KrakenousException(Exception):
+    pass
+
+
+class KrakenousNotImplemented(Exception):
     pass
