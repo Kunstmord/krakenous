8efd5e4729e2291d7f136b2fc50f56a3c3aaa164
georgeoblapenko <kunstmord@gmail.com>
2014-12-30 17:42:02 +0300

    numerous fixes and additions

--- krakenlib/backend_sqlite.py
+++ krakenlib/backend_sqlite.py
@@ -42,33 +42,29 @@ def create_new_column(db: dict, data_name: str):
 
 
 def write_data(db: dict, record_id: int, data_name: str, data):
-    pass
-#
-#
-# def write_data(db: dict, record_id: int, data_name: str, data):
-#     """Write data for a given record_id
-#     """
-#
-#     if not db['db'].keys():
-#         db['db'][str(record_id)] = {}
-#     elif str(record_id) not in db['db']:
-#         db['db'][str(record_id)] = {}
-#     if not db['db'][str(record_id)].keys():
-#         db['db'][str(record_id)] = {data_name: data}
-#     else:
-#         if db['writeback']:
-#             db['db'][str(record_id)][data_name] = data
-#         else:
-#             tmp_dict = db['db'][str(record_id)]
-#             tmp_dict[data_name] = data
-#             db['db'][str(record_id)] = tmp_dict
+    sql_string = 'UPDATE ' + db['table_name'] + ' SET ' + data_name + '=' + dumps(data) + ' WHERE id=' + str(record_id)
+    db['db'].execute(sql_string)
+
+
+def append_data_record(db: dict, record_id: int, data_dict: dict):
+    c_names = '('
+    c_vals = '('
+    for k in data_dict.keys():
+        c_names += k + ', '
+        c_vals += dumps(data_dict[k]) + ', '
+    c_names = c_names[:-2] + ')'
+    c_vals = c_vals[:-2] + ')'
+    sql_string = 'INSERT INTO ' + db['table_name'] + c_names + ' VALUES ' + c_vals
+    db['db'].execute(sql_string)
 
 
 def read_single_data(db: dict, record_id: int, data_name: str):
     """Return the contents of a data column specified by data_name for a given record_id
     """
-    db['db'].execute('SELECT :data_name FROM :tablename WHERE id=:record_id',
-                     {'data_name': data_name, 'tablename': db['table_name'], 'record_id': record_id})
+    sql_string = 'SELECT ' + data_name + ' FROM ' + db['table_name'] + ' WHERE id=' + str(record_id)
+    # db['db'].execute('SELECT :data_name FROM :tablename WHERE id=:record_id',
+    #                  {'data_name': data_name, 'tablename': db['table_name'], 'record_id': record_id})
+    db['db'].execute(sql_string)
     return loads(db['db'].fetchone()[0])
 
 
@@ -85,9 +81,10 @@ def read_all_data(db: dict, record_id: int):
     """
     Return the contents of all data columns for a given record_id
     """
-    db['db'].execute('SELECT * FROM :tablename WHERE id=:record_id', {'tablename': db['table_name'],
-                                                                      'record_id': record_id})
-
+    sql_string = 'SELECT * FROM ' + db['table_name'] + ' WHERE id=' + str(record_id)
+    # db['db'].execute('SELECT * FROM :tablename WHERE id=:record_id', {'tablename': db['table_name'],
+    #                                                                   'record_id': record_id})
+    db['db'].execute(sql_string)
     res = db['db'].fetchone()
     return list(map(lambda x: loads(x), res[1:]))
 
@@ -95,9 +92,11 @@ def read_all_data(db: dict, record_id: int):
 def data_exists(db: dict, record_id: int, data_name: str) -> bool:
     """Check if data exists for a given column name and record_id
     """
-    db['db'].execute('SELECT :data_name FROM :tablename WHERE id=:record_id', {'data_name': data_name,
-                                                                               'tablename': db['table_name'],
-                                                                               'record_id': record_id})
+    sql_string = 'SELECT ' + data_name + ' FROM ' + db['table_name'] + ' WHERE id=' + str(record_id)
+    # db['db'].execute('SELECT :data_name FROM :tablename WHERE id=:record_id', {'data_name': data_name,
+    #                                                                            'tablename': db['table_name'],
+    #                                                                            'record_id': record_id})
+    db['db'].execute(sql_string)
     if not db['db'].fetchone()[0]:
         return False
     else:
@@ -105,10 +104,13 @@ def data_exists(db: dict, record_id: int, data_name: str) -> bool:
 
 
 def all_data_names(db: dict) -> list:
-    """Return data column names
+    """Return data column names (doesn't include id)
     """
-    db['db'].execute('SELECT * FROM :tablename', {'tablename': db['table_name']})
-    return list(map(lambda x: x[0], db['db'].description))
+    sql_string = 'SELECT * FROM ' + db['table_name']
+    # sql_string = 'CREATE TABLE ' + db_data['table_name'] + ' (id integer primary key)'
+    db['db'].execute(sql_string)
+    # db['db'].execute('SELECT * FROM :tablename', {'tablename': db['table_name']})
+    return list(map(lambda x: x[0], db['db'].description))[1:]
 
 
 # def delete_record(db: dict, record_id: int, total_records):
@@ -127,7 +129,9 @@ def data_records_amount(db_data: dict) -> int:
     """
     conn = sqlite3.connect(db_data['db_path'])
     c = conn.cursor()
-    c.execute('SELECT Count(*) FROM :tablename', {'tablename': db_data['table_name']})
+    sql_string = 'SELECT Count(*) FROM ' + db_data['table_name']
+    # c.execute('SELECT Count(*) FROM :tablename', {'tablename': db_data['table_name']})
+    c.execute(sql_string)
     res = c.fetchone()[0]
     conn.close()
     return res
