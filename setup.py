a21c4d4383a6ffdae5c85fc04cc9c64be5eca7e1
georgeoblapenko <kunstmord@gmail.com>
2015-01-01 22:06:59 +0300

    v0.1

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from distutils.core import setup
 
-setup(name='krakenlib',
+setup(name='krakenous',
       version='0.1',
       description='A backend for machine learning-related feature extraction and storing',
       author='Viktor Evstratov, George Oblapenko',
       author_email='kunstmord@kunstmord.com',
       url='https://github.com/Kunstmord/krakenlib',
       license="GPL",
-      packages=['krakenlib'],
-      package_dir={'krakenlib': 'krakenlib'},
+      packages=['krakenous'],
+      package_dir={'krakenous': 'krakenlib'},
       requires=['numpy'],
       )
