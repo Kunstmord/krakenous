40e1974399f166002fc69772b126268971ebf06a
georgeoblapenko <kunstmord@gmail.com>
2015-01-07 22:36:11 +0300

    removed shelve-specific stuff (yes, we're deprecating the shelve backend)

--- krakenous/dataset.py
+++ krakenous/dataset.py
@@ -14,16 +14,7 @@ class DataSet(object):
             raise KrakenousException('No backend specified')
         else:
             self.backend_name = kwargs['backend']
-
-        if self.backend_name == 'shelve':
-            if 'db_path' not in kwargs:
-                raise KrakenousException('No path to shelve file specified')
-            else:
-                self.db_data = {'db_path': kwargs['db_path']}
-                self.backend = __import__('krakenous.backend_shelve', fromlist=['backend_shelve'])
-                if os.path.isfile(self.db_data['db_path'] + '.db'):
-                    self.total_records = self.backend.data_records_amount(self.db_data)
-        elif self.backend_name == 'sqlite':
+        if self.backend_name == 'sqlite':
             if 'db_path' not in kwargs:
                 raise KrakenousException('No path to SQLite file specified')
             elif 'table_name' not in kwargs:
@@ -57,20 +48,15 @@ class DataSet(object):
         tentacle is a generator, returns a dictionary of stuff
         """
         records_added = 0
-        if self.backend_name in ('shelve', 'sqlite',):
-            if writeback != 0:
-                db = self.backend.open_db(self.db_data, True)
-            else:
-                db = self.backend.open_db(self.db_data, False)
+        db = self.backend.open_db(self.db_data)
         for tentacle_dict in tentacle(*args, **kwargs):
-            if self.backend_name == 'sqlite':
-                try:
-                    all_names = self.feature_names()
-                except KrakenousException:
-                    all_names = []
-                for tentacle_key in tentacle_dict:
-                    if tentacle_key not in all_names:
-                        self.backend.create_new_column(db, tentacle_key)
+            try:
+                all_names = self.feature_names()
+            except KrakenousException:
+                all_names = []
+            for tentacle_key in tentacle_dict:
+                if tentacle_key not in all_names:
+                    self.backend.create_new_column(db, tentacle_key)
             self._append_data_record_open(tentacle_dict, db)
             records_added += 1
             if writeback != 0 and self.total_records % writeback == 0:
@@ -82,22 +68,17 @@ class DataSet(object):
         """
         do not write id field! (if exists in dict) (doesn't concern shelve)
         """
-        if self.backend_name in ('shelve', 'sqlite',):
-            db = self.backend.open_db(self.db_data)
-        if self.backend_name == 'shelve':
-            for data_entry in data_record:
-                self.backend.write_data(db, self.total_records + 1, data_entry, data_record[data_entry])
-        elif self.backend_name == 'sqlite':
-            if 'id' in data_record.keys():
-                raise KrakenousException('Cannot write to id column')
-            try:
-                all_names = self.feature_names()
-            except KrakenousException:
-                all_names = []
-            for data_key in data_record:
-                if data_key not in all_names:
-                    self.backend.create_new_column(db, data_key)
-            self.backend.append_data_record(db, 1, data_record, serializers)
+        db = self.backend.open_db(self.db_data)
+        if 'id' in data_record.keys():
+            raise KrakenousException('Cannot write to id column')
+        try:
+            all_names = self.feature_names()
+        except KrakenousException:
+            all_names = []
+        for data_key in data_record:
+            if data_key not in all_names:
+                self.backend.create_new_column(db, data_key)
+        self.backend.append_data_record(db, 1, data_record, serializers)
         self.total_records += 1
         self.backend.close_db(db)
 
@@ -108,8 +89,6 @@ class DataSet(object):
         self.total_records += 1
 
         self.backend.append_data_record(db, self.total_records, data_record)
-        # for data_entry in data_record:
-        #     backend.write_data(db, self.total_records, data_entry, data_record[data_entry])
 
     def insert_single(self, record_id: int, feature_name: str, data, overwrite_existing: bool=False, serializer=None):
         """
@@ -118,17 +97,11 @@ class DataSet(object):
         if not overwrite_existing and self.feature_exists(record_id, feature_name):
             raise KrakenousException('Feature with name "' + feature_name + '" already exists')
         db = self.backend.open_db(self.db_data)
-        if self.backend_name == 'sqlite':
-            if not serializer:
-                serializer = dumps
-            if feature_name not in self.feature_names():
-                self.backend.create_new_column(db, feature_name)
-        if self.backend_name == 'sqlite' and feature_name not in self.feature_names():
+        if not serializer:
+            serializer = dumps
+        if feature_name not in self.feature_names():
             self.backend.create_new_column(db, feature_name)
-        if self.backend_name == 'shelve':
-            self.backend.write_data(db, record_id, feature_name, data)
-        else:
-            self.backend.write_data(db, record_id, feature_name, data, serializer)
+        self.backend.write_data(db, record_id, feature_name, data, serializer)
         self.backend.close_db(db)
 
     def insert_for_range(self, start_id: int, end_id: int, feature_name: str, data,
@@ -137,14 +110,14 @@ class DataSet(object):
         insert a single value (data) for a range of ids
         """
         db = self.backend.open_db(self.db_data)
-        if self.backend_name == 'sqlite':
-            if not serializer:
-                serializer = dumps
-            if feature_name not in self.feature_names():
-                self.backend.create_new_column(db, feature_name)
-            else:
-                if not overwrite_existing:
-                    raise KrakenousException('Feature with name "' + feature_name + '" already exists')
+        # if self.backend_name == 'sqlite':
+        if not serializer:
+            serializer = dumps
+        if feature_name not in self.feature_names():
+            self.backend.create_new_column(db, feature_name)
+        else:
+            if not overwrite_existing:
+                raise KrakenousException('Feature with name "' + feature_name + '" already exists')
         for i, record_id in enumerate(range(start_id, end_id + 1)):
             self.backend.write_data(db, record_id, feature_name, data[i], serializer)
         self.backend.close_db(db)
@@ -160,16 +133,12 @@ class DataSet(object):
             raise KrakenousException('Feature with name "' + extractor_name + '" already exists')
         if extractor_name == 'id':
             raise KrakenousException('Cannot write to field "id"')
-        if writeback != 0:
-            db = self.backend.open_db(self.db_data, True)
-        else:
-            db = self.backend.open_db(self.db_data, False)
+        db = self.backend.open_db(self.db_data)
 
-        if self.backend_name == 'sqlite':
-            if not global_existence:
-                self.backend.create_new_column(db, extractor_name)
-            if not serializer:
-                serializer = dumps
+        if not global_existence:
+            self.backend.create_new_column(db, extractor_name)
+        if not serializer:
+            serializer = dumps
 
         for record_id in range(start_id, end_id + 1):
             additional_data = {}
@@ -178,12 +147,8 @@ class DataSet(object):
                     additional_data[column_name] = self.backend.read_single_data(db, record_id, column_name)
                 else:
                     additional_data[column_name] = record_id
-            if self.backend_name == 'shelve':
-                self.backend.write_data(db, record_id, extractor_name, extractor(additional_data,
-                                                                                 *args, **kwargs))
-            elif self.backend_name == 'sqlite':
-                self.backend.write_data(db, record_id, extractor_name, extractor(additional_data,
-                                                                                 *args, **kwargs), serializer)
+            self.backend.write_data(db, record_id, extractor_name, extractor(additional_data,
+                                                                             *args, **kwargs), serializer)
 
             if verbose != 0 and record_id % verbose == 0:
                 print(record_id)
@@ -225,10 +190,7 @@ class DataSet(object):
         if self.total_records == 0:
             raise KrakenousException('The dataset is empty!')
         else:
-            if writeback != 0:
-                db = self.backend.open_db(self.db_data, True)
-            else:
-                db = self.backend.open_db(self.db_data, False)
+            db = self.backend.open_db(self.db_data)
             for record_id in range(self.total_records):
                 self.backend.delete_data(db, record_id + 1, feature_name)
                 if writeback != 0 and record_id % writeback == 0:
@@ -239,42 +201,30 @@ class DataSet(object):
                        writeback: int=0):
         if self.backend_name == 'sqlite':
             raise KrakenousUnsupportedOperation('Renaming of features is not available for the SQLite backend')
-        if self.total_records == 0:
-            raise KrakenousException('The dataset is empty!')
-        if not overwrite_existing and self.feature_exists_global(new_feature_name):
-            raise KrakenousException('Feature with name "' + new_feature_name + '" already exists')
-
-        if self.backend_name == 'shelve':
-            if writeback != 0:
-                db = self.backend.open_db(self.db_data, True)
-            else:
-                db = self.backend.open_db(self.db_data, False)
-            for record_id in range(self.total_records):
-                self.backend.write_data(db, record_id + 1, new_feature_name, self.backend.read_single_data(db, record_id + 1,
-                                                                                          original_feature_name))
-                self.backend.delete_data(db, record_id + 1, original_feature_name)
-                if writeback != 0 and record_id % writeback == 0:
-                    self.backend.commit_db(db)
-            self.backend.close_db(db)
+        # if self.total_records == 0:
+        #     raise KrakenousException('The dataset is empty!')
+        # if not overwrite_existing and self.feature_exists_global(new_feature_name):
+        #     raise KrakenousException('Feature with name "' + new_feature_name + '" already exists')
 
     def delete_records(self, record_ids: tuple, writeback: int=0):
-        if self.backend_name == 'shelve':
-            if writeback != 0:
-                db = self.backend.open_db(self.db_data, True)
-            else:
-                db = self.backend.open_db(self.db_data, False)
-        elif self.backend_name == 'sqlite':
-            raise KrakenousNotImplemented('Deleting records is not implemented for the SQLite backend')
-        if self.total_records == 0:
-            raise KrakenousException('The dataset is empty!')
-        for record_id in record_ids:
-            if record_id > self.total_records:
-                raise KrakenousException('Record with id=' + str(record_id) + ' does not exist!')
-            else:
-                self.backend.delete_record(db, record_id, self.total_records)
-                if writeback != 0 and record_id % writeback == 0:
-                    self.backend.commit_db(db)
-                self.total_records -= 1
+        raise KrakenousNotImplemented('Deleting records is not implemented for the SQLite backend')
+        # if self.backend_name == 'shelve':
+        #     if writeback != 0:
+        #         db = self.backend.open_db(self.db_data, True)
+        #     else:
+        #         db = self.backend.open_db(self.db_data, False)
+        # elif self.backend_name == 'sqlite':
+        #     raise KrakenousNotImplemented('Deleting records is not implemented for the SQLite backend')
+        # if self.total_records == 0:
+        #     raise KrakenousException('The dataset is empty!')
+        # for record_id in record_ids:
+        #     if record_id > self.total_records:
+        #         raise KrakenousException('Record with id=' + str(record_id) + ' does not exist!')
+        #     else:
+        #         self.backend.delete_record(db, record_id, self.total_records)
+        #         if writeback != 0 and record_id % writeback == 0:
+        #             self.backend.commit_db(db)
+        #         self.total_records -= 1
 
     def force_write_feature(self, feature_name: str, feature: list, writeback: int=0):
         if len(feature) != self.total_records:
@@ -283,10 +233,7 @@ class DataSet(object):
         elif feature_name == 'id':
             raise KrakenousException('Cannot write to field "id"')
         else:
-            if writeback != 0:
-                db = self.backend.open_db(self.db_data, True)
-            else:
-                db = self.backend.open_db(self.db_data, False)
+            db = self.backend.open_db(self.db_data)
             for record_id in range(self.total_records):
                 self.backend.write_data(db, record_id + 1, feature_name, feature[record_id])
                 if writeback != 0 and self.total_records % writeback == 0:
@@ -295,7 +242,7 @@ class DataSet(object):
 
     def feature_exists(self, record_id: int, feature_name: str) -> bool:
         db = self.backend.open_db(self.db_data)
-        if self.backend_name == 'sqlite' and feature_name not in self.feature_names():
+        if feature_name not in self.feature_names():
             self.backend.close_db(db)
             return False
         else:
@@ -319,12 +266,8 @@ class DataSet(object):
             raise KrakenousException('Feature "' + feature_name + '" does not exist')
         db = self.backend.open_db(self.db_data)
         result = []
-        if self.backend_name == 'sqlite':
-            for record_id in range(start_id, end_id + 1):
-                result.append(self.backend.read_single_data(db, record_id, feature_name, deserializer))
-        else:
-            for record_id in range(start_id, end_id + 1):
-                result.append(self.backend.read_single_data(db, record_id, feature_name))
+        for record_id in range(start_id, end_id + 1):
+            result.append(self.backend.read_single_data(db, record_id, feature_name, deserializer))
         self.backend.close_db(db)
         return result
 
@@ -340,31 +283,16 @@ class DataSet(object):
                 raise KrakenousException('Feature "' + feature_name + '" does not exist')
         db = self.backend.open_db(self.db_data)
         result = []
-        if self.backend_name == 'sqlite':
-            for record_id in range(start_id, end_id + 1):
-                result.append(self.backend.read_multiple_data(db, record_id, feature_names, deserializers))
-        else:
-            for record_id in range(start_id, end_id + 1):
-                result.append(self.backend.read_multiple_data(db, record_id, feature_names))
+        for record_id in range(start_id, end_id + 1):
+            result.append(self.backend.read_multiple_data(db, record_id, feature_names, deserializers))
         self.backend.close_db(db)
         return result
 
-    def feature_names(self, record_id=-1) -> list:
+    def feature_names(self) -> list:
         if self.total_records == 0:
             raise KrakenousException('The dataset is empty!')
         db = self.backend.open_db(self.db_data)
-        return_list = []
-        if self.backend_name == 'shelve':
-            if record_id == -1:
-                for r_id in range(self.total_records):
-                    tmp_list = self.backend.all_data_names(db, r_id + 1)
-                    for tmp_f in tmp_list:
-                        if tmp_f not in return_list:
-                            return_list.append(tmp_f)
-            else:
-                return_list = self.backend.all_data_names(db, record_id)
-        elif self.backend_name == 'sqlite':
-            return_list = self.backend.all_data_names(db)
+        return_list = self.backend.all_data_names(db)
         self.backend.close_db(db)
         return return_list
 
@@ -395,28 +323,21 @@ class DataSet(object):
         result_dict = {}
         db = self.backend.open_db(self.db_data)
         if column_names == ():
-            if self.backend_name == 'shelve':
-                result_dict = self.backend.read_all_data(db, record_id)
-            elif self.backend_name == 'sqlite':
-                tmp_res = self.backend.read_all_data(db, record_id, deserializers)
-                all_column_names = self.backend.all_data_names(db)
-                for column_name_enum in enumerate(all_column_names):
-                    result_dict[column_name_enum[1]] = tmp_res[column_name_enum[0]]
+            tmp_res = self.backend.read_all_data(db, record_id, deserializers)
+            all_column_names = self.backend.all_data_names(db)
+            for column_name_enum in enumerate(all_column_names):
+                result_dict[column_name_enum[1]] = tmp_res[column_name_enum[0]]
             result_dict['id'] = record_id
         else:
             if 'id' in column_names:
                 result_dict['id'] = record_id
-            if self.backend_name == 'shelve':
-                for column_name in column_names:
-                    result_dict[column_name] = self.backend.read_single_data(db, record_id, column_name)
-            elif self.backend_name == 'sqlite':
-                for column_name in column_names:
-                    if column_name != 'id':
-                        if deserializers and column_name in deserializers:
-                            result_dict[column_name] = self.backend.read_single_data(db, record_id, column_name,
-                                                                                     deserializers[column_name])
-                        else:
-                            result_dict[column_name] = self.backend.read_single_data(db, record_id, column_name)
+            for column_name in column_names:
+                if column_name != 'id':
+                    if deserializers and column_name in deserializers:
+                        result_dict[column_name] = self.backend.read_single_data(db, record_id, column_name,
+                                                                                 deserializers[column_name])
+                    else:
+                        result_dict[column_name] = self.backend.read_single_data(db, record_id, column_name)
         self.backend.close_db(db)
         return result_dict
 
