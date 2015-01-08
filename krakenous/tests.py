06270c6504c6077278b7cfaa1f3ca9791dd39beb
georgeoblapenko <kunstmord@gmail.com>
2015-01-08 17:08:26 +0300

    removed shelve backend-related tests, cleanup

--- krakenous/tests.py
+++ krakenous/tests.py
@@ -71,12 +71,7 @@ def folder_destroyer():
 
 class BaseTest(unittest.TestCase):
     def setUp(self):
-        """
-        This creates a DataSet which uses the shelve backend. It also creates a folder with 10 files (named 0 to 9)
-        to use with the folder_tentacle (if a testfolder already exists, this just reaches inside it and grabs what's
-        there)
-        """
-        self.dataset = DataSet(backend='shelve', db_path='testshelve')  # we will use the folder tentacle for testing
+        self.dataset = DataSet(backend='sqlite', db_path='testsqlite', table_name='test_table')
         if self.dataset.total_records == 0:
             folder_creator()
             self.dataset.populate(folder_tentacle, 'testfolder', 'filename')
@@ -130,68 +125,15 @@ class BaseTest(unittest.TestCase):
         assert feature[2] == 6
 
     def test_filter(self):
-        tests = list(self.dataset.yield_data_records(('id',), filters={'id': 4}))
-        assert len(tests) == 1
-        tests = list(self.dataset.yield_data_records(('id',), filters={'id': 114}))
-        assert len(tests) == 0
         self.dataset.extract_feature_simple(id_heavyside, ('id',))
         tests = list(self.dataset.yield_data_records(('id_heavyside',), filters={'id_heavyside': 0}))
         assert len(tests) == 5
 
-    def tearDown(self):
-        folder_destroyer()
-        remove('testshelve.db')
+        tests = list(self.dataset.yield_data_records(('id',), filters={'id': 4}))
+        assert len(tests) == 1
 
-
-class TestShelve(BaseTest):
-    def test_delete_feature(self):
-        self.dataset.extract_feature_full(a_very_fake_extractor, (), overwrite_feature=True)
-        assert self.dataset.feature_exists_global('a_very_fake_extractor') is True  # feature exists
-        self.dataset.delete_feature('a_very_fake_extractor')
-        assert self.dataset.feature_exists_global('a_very_fake_extractor') is False  # featue doesn't exist anymore
-
-    def test_rename_feature(self):
-        self.dataset.extract_feature_full(a_very_fake_extractor, (), overwrite_feature=True)
-        self.dataset.extract_feature_full(ext3, (), overwrite_feature=True)
-        assert self.dataset.feature_exists_global('a_very_fake_extractor') is True  # feature exists
-        self.dataset.rename_feature('a_very_fake_extractor', 'renamed_fake_extractor')
-        assert self.dataset.feature_exists_global('a_very_fake_extractor') is False  # original name doesn't exists
-        assert self.dataset.feature_exists_global('renamed_fake_extractor') is True  # new name exists
-        assert self.dataset.single_data_record(3, ('ext3',))['ext3'] == 23930  # original value is correct
-        self.dataset.rename_feature('renamed_fake_extractor', 'ext3', overwrite_existing=True, writeback=3)
-        assert self.dataset.feature_exists_global('renamed_fake_extractor') is False  # new name doesn't exist
-        assert self.dataset.feature_exists_global('ext3') is True  # newest name still exists
-        assert self.dataset.single_data_record(3, ('ext3',))['ext3'] == 30239  # and the value was overwritten
-
-    def test_feature_names(self):
-        self.dataset.extract_feature_full(a_very_fake_extractor, (), overwrite_feature=True)
-        self.dataset.extract_feature_full(numpy_feature, (), overwrite_feature=True)
-        self.dataset.extract_feature_full(string_feature, (), overwrite_feature=True)
-        feature_names = self.dataset.feature_names()
-        assert 'a_very_fake_extractor' in feature_names
-        assert 'string_feature' in feature_names
-        assert 'numpy_feature' in feature_names  # the features were returned
-
-    def test_append_and_delete_data_record(self):
-        current_records = self.dataset.total_records
-        self.dataset.append_data_record({'fake_extractor': 'NOT THE MEANING OF LIFE AT ALL'})
-        assert self.dataset.total_records == current_records + 1  # the number of records changes
-        assert self.dataset.single_data_record(current_records + 1, ('fake_extractor',))['fake_extractor']\
-               == 'NOT THE MEANING OF LIFE AT ALL'  # the data in the record is correct
-        assert self.dataset.single_data_record(2, ('fake_extractor',))['fake_extractor']\
-               != 'NOT THE MEANING OF LIFE AT ALL'  # the data in other record doesn't change
-        self.dataset.delete_records((self.dataset.total_records,))
-        assert self.dataset.total_records == current_records  # the number of records changes back to the original
-        assert self.dataset.single_data_record(current_records, ('fake_extractor',))['fake_extractor']\
-               != 'NOT THE MEANING OF LIFE AT ALL'  # the last record is correct
-
-
-class TestSQLite(BaseTest):
-    def setUp(self):
-        self.dataset = DataSet(backend='sqlite', db_path='testsqlite', table_name='test_table')
-        if self.dataset.total_records == 0:
-            folder_creator()
-            self.dataset.populate(folder_tentacle, 'testfolder', 'filename')
+        tests = list(self.dataset.yield_data_records(('id',), filters={'id': 114}))
+        assert len(tests) == 0
 
     def test_deserialize(self):
         self.dataset.extract_feature_simple_custom_serializer(numpy_feature_md, (), numpy_array_serializer)
@@ -212,6 +154,10 @@ class TestSQLite(BaseTest):
     def tearDown(self):
         folder_destroyer()
         remove('testsqlite')
+
+
+class TestSQLite(BaseTest):
+    pass
 
 
 class TestNumpyConvert(unittest.TestCase):
